639af9aa9b4df3008c378cb460dd7b77bb0d4d25
redwoodcutter <oguzkumcular@gmail.com>
2022-03-08 01:31:42 +0300

    First Test Driven C

---
+++ First_Unit_Test/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace First_Unit_Test
+{
+    public class Calculator
+    {
+        public int AddNumbers(int a , int b)
+        {
+            return a + b;
+        }
+
+    }
+}
