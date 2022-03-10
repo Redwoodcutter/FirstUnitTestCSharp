43a8b8bb67c96e64289ff7cec55d363a868d9037
redwoodcutter <oguzkumcular@gmail.com>
2022-03-10 21:06:28 +0300

    Create Nunit Test

---
+++ First_NUnit_Test/CalculatorNunitTests.cs
@@ -0,0 +1,26 @@
+﻿using First_Unit_Test;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_NUnit_Test
+{
+    [TestFixture]
+    public class CalculatorNunitTests
+    {
+        [Test]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            Calculator calc = new Calculator();
+            //Act
+            int result = calc.AddNumbers(10, 20);
+            //Assert
+            Assert.AreEqual(30, result);
+        }
+
+    }
+}
