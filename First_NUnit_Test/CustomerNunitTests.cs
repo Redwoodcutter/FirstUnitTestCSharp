3635db955c86a57d0f3921a6c06615abae2266e0
redwoodcutter <oguzkumcular@gmail.com>
2022-03-13 00:17:06 +0300

    Add String Test

---
+++ First_NUnit_Test/CustomerNunitTests.cs
@@ -0,0 +1,25 @@
+﻿using First_Unit_Test;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_NUnit_Test
+{
+    [TestFixture]
+    public class CustomerNunitTests
+    {
+        [Test]
+        public void FirstLastName_ReturnFullName()
+        {
+            //Arrange
+            var customer = new Customer();
+            //Act
+            string fullName = customer.GreatingWithNames("Oguz", "Kumcular");
+            //Assert
+            Assert.That(fullName, Is.EqualTo("Hello, Oguz Kumcular"));
+        }
+    }
+}
---
+++ First_Unit_Test/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace First_Unit_Test
+{
+    public class Customer
+    {
+        public string GreatingWithNames ( string firstName, string lastName)
+        {
+            return $"Hello, {firstName} {lastName}";
+        }
+    }
+}
