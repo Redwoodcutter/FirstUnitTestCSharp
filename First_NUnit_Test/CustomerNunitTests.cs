889d3f3ae44ecabe81b9192852361aec720a043e
redwoodcutter <oguzkumcular@gmail.com>
2022-03-13 01:41:19 +0300

    Add String Test Helper

--- First_NUnit_Test/CustomerNunitTests.cs
+++ First_NUnit_Test/CustomerNunitTests.cs
@@ -19,7 +19,13 @@ namespace First_NUnit_Test
             //Act
             string fullName = customer.GreatingWithNames("Oguz", "Kumcular");
             //Assert
+            Assert.AreEqual(fullName, "Hello, Oguz Kumcular");
             Assert.That(fullName, Is.EqualTo("Hello, Oguz Kumcular"));
+            Assert.That(fullName, Does.Contain("oguz Kumcular").IgnoreCase);
+            Assert.That(fullName, Does.StartWith("Hello,"));
+            Assert.That(fullName, Does.EndWith("Kumcular"));
+            Assert.That(fullName, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]"));
+
         }
     }
 }
